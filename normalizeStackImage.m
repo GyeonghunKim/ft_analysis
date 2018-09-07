1b54fb883bed14fd84c6d60de54c36a3ea1d5042
GHKim <aass98998@snu.ac.kr>
2018-09-07 16:02:56 +0900

    function for normalize stacked image by dividing local sum

---
+++ normalizeStackImage.m
@@ -0,0 +1,9 @@
+function norm_stacked_image = normalizeStackImage(stacked_peak, subsize)
+    norm_stacked_image = zeros(size(stacked_peak));
+    for x = sub_size+1:511-sub_size
+        for y = sub_size+1:511-sub_size
+            temp = sum(sum(stacked_peak(x-sub_size:x+sub_size, y-sub_size:y+sub_size)));
+            norm_stacked_image(x,y) = stacked_peak(x,y)/(temp/((2*sub_size +1)*(2*sub_size +1)));
+        end
+    end
+end
