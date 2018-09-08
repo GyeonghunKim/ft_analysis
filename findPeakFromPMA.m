20a080849739b5750a84149fa4e496c9fbc724f5
GHKim <aass98998@snu.ac.kr>
2018-09-08 20:12:46 +0900

    add handles for input parameter for GUI

--- findPeakFromPMA.m
+++ findPeakFromPMA.m
@@ -1,7 +1,11 @@
-function peak = findPeakFromPMA(c, filename)
-
+function peak = findPeakFromPMA(c, filename, handles)
+if nargin == 3
+    axes(handles.axes1);
+    nargin
+end
 %% import file
 addpath(c)
+
 file_name = strcat(c, "/",filename);
 
 fid_pma = fopen(file_name,'r');
@@ -18,7 +22,6 @@ peak = zeros(2,1);
 for i=1:film_length
     
     one_frame = fread(fid_pma,[ysize,xsize], 'uint8');
-    figure(1)
     imagesc(one_frame');
     peak = [peak;FastPeakFind(one_frame')];
     title(sprintf('%d',i));
@@ -28,5 +31,5 @@ for i=1:film_length
         disp(sprintf('%d/%d',i,film_length));
     end
 end
-
+fclose('all');
 end
